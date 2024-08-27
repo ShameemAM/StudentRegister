690b888d66d4fb3a05b1cb242a194e808753c0c8
ShameemAM <sashameem99@outlook.com>
2024-08-27 12:12:43 +0530

    students and hobbies controllers added

---
+++ StudentRegister/Controllers/HobbiesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentRegister.API.Controllers
+{
+    public class HobbiesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
